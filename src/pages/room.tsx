9de5d674bec7d843e2db6245e33c0bf2e8729c91
Pedro Galembeck <pedro.galembeck@mblabs.com.br>
2025-07-08 12:32:40 -0300

    feat: create initial project structure

---
+++ src/pages/room.tsx
@@ -0,0 +1,12 @@
+import { Navigate, useParams } from 'react-router-dom';
+import type { RoomParams } from '@/types';
+
+export function Room() {
+  const params = useParams<RoomParams>();
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />;
+  }
+
+  return <div>Room Details {JSON.stringify(params)}</div>;
+}
---
+++ src/types/index.d.ts
@@ -0,0 +1,8 @@
+export type GetRoomsAPIResponse = Array<{
+  id: string;
+  name: string;
+}>;
+
+export type RoomParams = {
+  roomId: string;
+};
